09b1efc132b1b36fadffb73a16daa0b8e05c4003
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2026-04-09 21:29:05 +0800

    feat(routes): add POS and Products screens to application routing

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router';
 import { Root } from '../components/Root';
 import { AuthScreen } from '../pages/AuthScreen';
+import { GrillSidePOS } from '../pages/POS';
+import { ProductsManager } from '../pages/ProductsManager';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +12,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     Component: Root,
-    children: [],
+    children: [
+      { path: 'pos', Component: GrillSidePOS },
+      { path: 'products', Component: ProductsManager },
+    ],
   },
 ]);
