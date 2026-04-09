d8d266de2a80d9e525738ac897e59b767ed54bb8
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2026-04-09 21:25:11 +0800

    feat(api): add PUT and DELETE methods to app.ts for product management

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
+import productRoutes from "./routes/productRoutes";
 
 const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 export default app;
