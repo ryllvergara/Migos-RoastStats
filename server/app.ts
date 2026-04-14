d42d8212c420878a7ae65130dfd2b8f2562acd4a
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2026-04-15 02:05:31 +0800

    feat(api): add PATCH method and register pos and management routes

--- server/app.ts
+++ server/app.ts
@@ -2,17 +2,21 @@ import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 import productRoutes from "./routes/productRoutes";
+import posRoutes from "./routes/posRoutes";
+import managementRoutes from "./routes/managementRoutes";
 
 const app = express();
 
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api", posRoutes);
+app.use("/api/management", managementRoutes);
 
 export default app;
