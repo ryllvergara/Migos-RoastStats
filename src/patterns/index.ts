038b41b8f2b16f2d2a7f866b4e6d992081e97f4f
ryllvergara <christineryll.vergara-24@cpu.edu.ph>
2026-05-05 23:03:51 +0800

    feat(patterns): implement SaleCommand for POS sale and undo actions using Command pattern

--- src/patterns/index.ts
+++ src/patterns/index.ts
@@ -1,9 +1,12 @@
 import { AppConfig } from "./appConfig";
 import { resolveCardView } from "./branchCards";
+import { executeSale, undoLastSale } from "./saleCommand";
 
 export { 
     AppConfig, 
     resolveCardView, 
+    executeSale,
+    undoLastSale
 };
 
 
